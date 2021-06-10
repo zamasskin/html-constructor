d1aee31ca8910d9d6b55055707bd050cfb30a746
Alexey Zamasskin <zamasskin@yandex.ru>
2021-06-10 12:43:26 +0300

    URL trait

---
+++ src/HtmlConstructor/UrlTags.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace HtmlConstructor;
+
+trait UrlTags {
+    static $TARGET_SELF = "_self";
+    static $TARGET_BLANK = "_blank";
+    static $TARGET_PARENT = "_parent";
+    static $TARGET_TOP = "_top";
+
+    /**
+     * UrlTags constructor.
+     * @param string $href
+     */
+    function __construct($href)
+    {
+        if(gettype($href) === "string" && strlen($href) > 0) {
+            $this->arParams["attributes"]["href"] = $href;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $href
+     * @return $this
+     */
+    function href($href) {
+        $this->arParams["attributes"]["href"] = $href;
+        return $this;
+    }
+
+    /**
+     * @param self::TARGET_*|string $target
+     * @return $this
+     */
+    function target($target) {
+        $this->arParams["attributes"]["target"] = $target;
+        return $this;
+    }
+}
