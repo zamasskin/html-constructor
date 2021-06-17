48c47600f594fdfc3d0cc4f322edaa33e5173087
Alexey Zamasskin <zamasskin@yandex.ru>
2021-06-17 19:13:03 +0300

    error test

--- tests/EmbeddedContentTest.php
+++ tests/EmbeddedContentTest.php
@@ -2,11 +2,11 @@
 use HtmlConstructor\Tags;
 use PHPUnit\Framework\TestCase;
 
-class EmbeddedContent extends TestCase {
+class EmbeddedContentTest extends TestCase {
     public function testEmbed() {
         $type = "video/webm";
         $src = "/media/cc0-videos/flower.mp4";
-        $render = sprintf('<img type="%s" src="%s" />', $type, $src);
+        $render = sprintf('<embed type="%s" src="%s" />', $type, $src);
         $this->assertSame(
             (new Tags\EmbeddedContent\Embed())->type($type)->src($src)->render(),
             $render
